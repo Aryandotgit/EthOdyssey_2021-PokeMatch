c8ff254e114e1b48ec64006f6a4172c5e7950df5
Aryandotgit <66879118+Aryandotgit@users.noreply.github.com>
2021-07-28 20:46:47 +0530

    front

---
+++ truffle-config.js
@@ -0,0 +1,38 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+
+require('babel-register');
+require('babel-polyfill');
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+
+    matic: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.maticvigil.com/`),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+
+
+ contracts_directory: './src/contracts/',
+  contracts_build_directory: 
+  compilers: {
+    solc: {
+      version: '0.6',
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
+}
