4c454d22149b837ed52c7cbfa1cc352b754c571c
Aryandotgit <66879118+Aryandotgit@users.noreply.github.com>
2021-07-28 20:54:34 +0530

    res

---
+++ src/components/App.js
@@ -0,0 +1,98 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React, { Component } from 'react';
+import Web3 from 'web3'
+import './App.css';
+import PokeToken from '../abis/PokeToken.json'
+import ball from '../ball.png'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '0x0',
+      token: null,
+      totalSupply: 0,
+      tokenURIs: [],
+      cardArray: [],
+      cardsChosen: [],
+      cardsChosenId: [],
+      cardsWon: []
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+          <a
+            className="navbar-brand col-sm-3 col-md-2 mr-0"
+            href="https://youtu.be/JuYeHPFR3f0"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          <img  src={brain} width="50" height="30" className="d-inline-block align-top" alt="" />
+          &nbsp; PokeMatch
+          </a>
+          <ul className="navbar-nav px-3">
+            <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+              <small className="text-muted"><span id="account">{this.state.account}</span></small>
+            </li>
+          </ul>
+        </nav>
+        <div className="container-fluid mt-5">
+          <div className="row">
+            <main role="main" className="col-lg-12 d-flex text-center">
+              <div className="content mr-auto ml-auto">
+                <h1 className="d-4">Start catching now! Match the Poke-Token!</h1>
+
+                <div className="grid mb-4" >
+
+                  { this.state.cardArray.map((card, key) => {
+                    return(
+                      <img
+                        key={key}
+                        src={this.chooseImage(key)}
+                        data-id={key}
+                        onClick={(event) => {
+                          let cardId = event.target.getAttribute('data-id')
+                          if(!this.state.cardsWon.includes(cardId.toString())) {
+                            this.flipCard(cardId)
+                          }
+                        }}
+                      />
+                    )
+                  })}
+
+
+                </div>
+
+                <div>
+
+                  <h5>Tokens Collected:<span id="result">&nbsp;{this.state.tokenURIs.length}</span></h5>
+
+                  <div className="grid mb-4" >
+
+                    { this.state.tokenURIs.map((tokenURI, key) => {
+                      return(
+                        <img
+                          key={key}
+                          src={tokenURI}
+                        />
+                      )
+                    })}
+
+                  </div>
+
+                </div>
+
+              </div>
+
+            </main>
+          </div>
+        </div>
+
+    );
+  }
+}
+
+export default App;
