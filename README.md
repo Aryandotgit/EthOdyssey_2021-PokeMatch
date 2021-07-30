801ecbd3032d5a3ac8d8dbe669b301821cf791f4
Aryandotgit <66879118+Aryandotgit@users.noreply.github.com>
2021-07-30 21:08:19 +0530

    frnt

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # EthOdyssey_2021
 EthOdyssey 2021 Project-PokeMatch
+
+# PokeMatch
+
+PokeMatch is a blockchain nft game inspired by the popular 90's Nintendo game Pokemon.
+The tokens inside the game are wide ranging from squirtle to jigglypuff, so be ready to dive into nostalgia!
+
+## Problems I faced
+
+
+## How To Play
+
+Simply connect your Metamask or any other web-wallet to the application and you are good to go!
+Your objective is to match similar tokens to get them.
+
+This is a demo test for the memory matching game which I have planned to expand to multiple levels filled with unique nfts for kids and people to collect just like you would do with your pokemon trading cards.
+
+## Technology used
+* Truffle
+* Metamask
+* Solidity
+* Git & Github
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,164 @@ import './App.css';
 import PokeToken from '../abis/PokeToken.json'
 import ball from '../ball.png'
 
+const CARD_ARRAY = [
+  {
+    name: 'bulbasaur',
+    img: '/images/bulbasaur.png'
+  },
+  {
+    name: 'eevee',
+    img: '/images/eevee.png'
+  },
+  {
+    name: 'jigglypuff',
+    img: '/images/jigglypuff.png'
+  },
+  {
+    name: 'meowth',
+    img: '/images/meowth.png'
+  },
+  {
+    name: 'psyduck',
+    img: '/images/psyduck.png'
+  },
+  {
+    name: 'squirtle',
+    img: '/images/squirtle.png'
+  },
+  {
+    name: 'bulbasaur',
+    img: '/images/bulbasaur.png'
+  },
+  {
+    name: 'eevee',
+    img: '/images/eevee.png'
+  },
+  {
+    name: 'jigglypuff',
+    img: '/images/jigglypuff.png'
+  },
+  {
+    name: 'meowth',
+    img: '/images/meowth.png'
+  },
+  {
+    name: 'psyduck',
+    img: '/images/psyduck.png'
+  },
+  {
+    name: 'squirtle',
+    img: '/images/squirtle.png'
+  }
+]
+
 class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+    this.setState({ cardArray: CARD_ARRAY.sort(() => 0.5 - Math.random()) })
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. Download MetaMask!')
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+
+    // Load smart contract
+    const networkId = await web3.eth.net.getId()
+    const networkData = PokeToken.networks[networkId]
+    if(networkData) {
+      const abi = PokeToken.abi
+      const address = networkData.address
+      const token = new web3.eth.Contract(abi, address)
+      this.setState({ token })
+      const totalSupply = await token.methods.totalSupply().call()
+      this.setState({ totalSupply })
+      // Load Tokens
+      let balanceOf = await token.methods.balanceOf(accounts[0]).call()
+      for (let i = 0; i < balanceOf; i++) {
+        let id = await token.methods.tokenOfOwnerByIndex(accounts[0], i).call()
+        let tokenURI = await token.methods.tokenURI(id).call()
+        this.setState({
+          tokenURIs: [...this.state.tokenURIs, tokenURI]
+        })
+      }
+    } else {
+      alert('Smart contract is not deployed to detected network.')
+    }
+  }
+
+  chooseImage = (cardId) => {
+    cardId = cardId.toString()
+    if(this.state.cardsWon.includes(cardId)) {
+      return window.location.origin + '/images/white.png'
+    }
+    else if(this.state.cardsChosenId.includes(cardId)) {
+      return CARD_ARRAY[cardId].img
+    } else {
+      return window.location.origin + '/images/ball.png'
+    }
+  }
+
+  flipCard = async (cardId) => {
+    let alreadyChosen = this.state.cardsChosen.length
+
+    this.setState({
+      cardsChosen: [...this.state.cardsChosen, this.state.cardArray[cardId].name],
+      cardsChosenId: [...this.state.cardsChosenId, cardId]
+    })
+
+    if (alreadyChosen === 1) {
+      setTimeout(this.checkForMatch, 100)
+    }
+  }
+
+
+  checkForMatch = async () => {
+    const optionOneId = this.state.cardsChosenId[0]
+    const optionTwoId = this.state.cardsChosenId[1]
+
+    if(optionOneId === optionTwoId) {
+      alert('You have clicked the same image!')
+    } else if (this.state.cardsChosen[0] === this.state.cardsChosen[1]) {
+      alert('You found a match')
+      this.state.token.methods.mint(
+        this.state.account,
+        window.location.origin + CARD_ARRAY[optionOneId].img.toString()
+      )
+      .send({ from: this.state.account })
+      .on('transactionHash', (hash) => {
+        this.setState({
+          cardsWon: [...this.state.cardsWon, optionOneId, optionTwoId],
+          tokenURIs: [...this.state.tokenURIs, CARD_ARRAY[optionOneId].img]
+        })
+      })
+    } else {
+      alert('Sorry, try again')
+    }
+    this.setState({
+      cardsChosen: [],
+      cardsChosenId: []
+    })
+    if (this.state.cardsWon.length === CARD_ARRAY.length) {
+      alert('Congratulations! You caught them all!')
+    }
+  }
+
   constructor(props) {
     super(props)
     this.state = {
@@ -68,7 +225,7 @@ class App extends Component {
 
                 <div>
 
-                  <h5>Tokens Collected:<span id="result">&nbsp;{this.state.tokenURIs.length}</span></h5>
+                  <h5>The Tokens Collected:<span id="result">&nbsp;{this.state.tokenURIs.length}</span></h5>
 
                   <div className="grid mb-4" >
 
@@ -90,10 +247,9 @@ class App extends Component {
             </main>
           </div>
         </div>
-
+      </div>
     );
   }
 }
-
 
 export default App;
